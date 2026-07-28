b8997a1dfc80760a700e60b49b72fd96d091b41a
yangcode <yanh814@foxmail.com>
2026-07-28 09:33:29 +0800

    回路重量均值添加

--- src/main/java/HarnessPackOpti/ProjectInfoOutPut/ProjectCircuitInfoOutput.java
+++ src/main/java/HarnessPackOpti/ProjectInfoOutPut/ProjectCircuitInfoOutput.java
@@ -1406,6 +1406,7 @@ public class ProjectCircuitInfoOutput {
         }
         totalCost.put("回路数量(打断前)", multiLoopInfosSet.size());
         totalCost.put("回路数量(打断后)", count);
+        totalCost.put("回路重量均值", Double.parseDouble(df.format(Double.parseDouble(totalCost.get("回路总重量").toString()) / multiLoopInfosSet.size())));
         // 打断前回路均值
         double avgLength = 0.00;
         if (multiLoopInfosSet.size() > 0) {
